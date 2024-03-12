512c80ff417bbdee99123e1c907e971a74d5b28b
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-12 17:19:57 +0100

    add slice

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -7,10 +7,7 @@ Chiediamo all'utente quanti elementi dell'array dovremo stampare
 Suggerimenti:
 Probabilmente conviene controllare che il numero di elementi della coda 
 non sia più grande del numero di elementi totali dell'array
-3.  (Bonus)
-Cartella: array_bonus
-Crea due array che hanno un numero di elementi diversi.
-Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
 */
 
 
@@ -20,17 +17,22 @@ nuButton.addEventListener('click' , function(){
     let numeri =[];
     let userNumber=document.getElementById('userNumber').value;
     console.log(userNumber);
-    let nuResult=document.getElementById('nuResult');
+    let errore=document.getElementById('errore');
     for(let i =1 ; i<=userNumber ; i++){
         if(userNumber>= 5){
             let randomNumber=getRndInteger(1, 100);
             numeri.push(randomNumber);
             console.log(numeri);
         }else{
-            nuResult.innerHTML='Devi inserire minimo 5 numeri!';
+            errore.innerHTML='Devi inserire minimo 5 numeri!';
         }
     }
     console.log(numeri);
+    let userChoice =document.getElementById('userChoice').value;
+    console.log(userChoice);
+    let lastFive= numeri.slice(-userChoice);
+    let nuResult = document.getElementById('nuResult');
+    nuResult.innerHTML = lastFive;
     
 })
 
